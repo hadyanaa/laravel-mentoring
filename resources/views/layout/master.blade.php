2a4575e60348f5c4d26c8dc4659ec8f8eabb1f52
hadyanaa <hadyanabdulaziz@gmail.com>
2022-12-23 15:51:04 +0700

    add icon page

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -18,6 +18,8 @@
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
+    <link rel="icon" href="/template/img/bkpk-sq.png" />
+
     <!-- Custom styles for this template-->
     <link href="{{asset('/template/css/sb-admin-2.min.css')}}" rel="stylesheet">
     <link href="{{asset('/template/css/app.css')}}" rel="stylesheet">
