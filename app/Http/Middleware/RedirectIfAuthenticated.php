3014459011cb6eb9e734482070bec2f3b2f19733
hadyanaa <hadyanabdulaziz@gmail.com>
2023-01-29 00:25:03 +0700

    add sweet alert after login

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -6,6 +6,7 @@ use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Tdanandeh\SweetAlert\SweetAlert;
 
 class RedirectIfAuthenticated
 {
@@ -23,7 +24,9 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                // return redirect(RouteServiceProvider::HOME);
+                SweetAlert::success('Berhasil login', 'Login');
+                return redirect('/');
             }
         }
 
